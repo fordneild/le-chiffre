362554a3a342b9581cdbdbf050d2ca013083690e
Ford Neild <fordneild@Fords-MacBook-Pro-2.local>
2019-10-28 20:19:07 -0400

    Updates

---
+++ precache-manifest.9eac22f82ff71b83b877fd21681835e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa55a0f7084ab0394dfb4f516aa0d87e",
+    "url": "/le-chiffre/index.html"
+  },
+  {
+    "revision": "0e82ae18ed89871706bd",
+    "url": "/le-chiffre/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d278acd337cba16c8f61",
+    "url": "/le-chiffre/static/js/2.79a3e44a.chunk.js"
+  },
+  {
+    "revision": "0e82ae18ed89871706bd",
+    "url": "/le-chiffre/static/js/main.17a7735d.chunk.js"
+  },
+  {
+    "revision": "9a055749ae0fc4a994fe",
+    "url": "/le-chiffre/static/js/runtime-main.726858e2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/le-chiffre/static/media/logo.25bf045c.svg"
+  }
+]);
